abe277a5063535399d31d9a52ce4531f58b0db08
therandomfool <mddenver@protonmail.com>
2020-11-19 17:16:56 -0800

    finished js

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Count Repeating Letters  JS
 
 - vanilla JS
+- counting letters in a row of a string
--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 //  string will not contain numbers or symbols  only letters
 
-function countLetters(str){
-    return str
+function countLetters(str) {
+    let tempArray = str.split('');
+    let letters = [];
+    let count = 1;
+
+    for (let i = 0; i < tempArray.length; i++) {
+        if (tempArray[i] === tempArray[i + 1]) {
+            count++
+        } else {
+            let value = `${count}${tempArray[i]}`;
+            letters = [...letters, value];
+            count = 1;
+        }
+    }
+    return letters.join("");
 }
 
-console.log(countLetters('ffffeerttttooogkhfiuefhhfks'));
+console.log(countLetters('ffffeerttttoooggggzzzzzzzz'));
